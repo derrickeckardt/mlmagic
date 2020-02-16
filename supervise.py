790591c734d2dd822cedbba8da9bddc8670ac3e8
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-16 13:16:33 +0000

    workig=ng on DT

--- supervise.py
+++ supervise.py
@@ -2,17 +2,24 @@
 import sys
 import pandas as pd
 import numpy as np
-import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn import tree 
 
 dataset, classcolumn, headers = sys.argv[1:]
 headers = None if headers == "None" else headers
 classcolumn = int(classcolumn) if headers == None else classcolumn
 
 data = pd.read_csv(dataset, header=headers)
-data.head()
+class_column = data[classcolumn]
+class_data = data.drop(classcolumn,1)
 
-data[classcolumn].head()
+x_train, x_test, y_train, y_test = train_test_split(class_data, class_column, test_size = 0.2)
 
-train_set, test_set = sk.model_selection.train_test_split()
+print(y_train.head())
+print(y_test.head())
 
-# Decision
+# Decision Trees
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_train,y_train)
+predictions = clf.predict(x_test,y_test)
+print(predictions)
