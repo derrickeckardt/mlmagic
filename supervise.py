90ca71a2a958c721489a61bcb08ee6cc9004e6c1
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-16 12:44:11 +0000

    adding dt's and update readmd requirements

--- supervise.py
+++ supervise.py
@@ -2,6 +2,7 @@
 import sys
 import pandas as pd
 import numpy as np
+import sklearn as sk
 
 dataset, classcolumn, headers = sys.argv[1:]
 headers = None if headers == "None" else headers
@@ -9,4 +10,9 @@ classcolumn = int(classcolumn) if headers == None else classcolumn
 
 data = pd.read_csv(dataset, header=headers)
 data.head()
-print(classcolumn)
+
+data[classcolumn].head()
+
+train_set, test_set = sk.model_selection.train_test_split()
+
+# Decision
