72f7c6c9924bca469f77a9a97e4ac0d8b86d8414
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-17 12:13:41 +0000

    trees added

--- supervise.py
+++ supervise.py
@@ -3,7 +3,8 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import tree 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 dataset, classcolumn, headers = sys.argv[1:]
 headers = None if headers == "None" else headers
@@ -13,14 +14,11 @@ data = pd.read_csv(dataset, header=headers)
 class_column = data[classcolumn]
 class_data = data.drop(classcolumn,1)
 
-x_train, x_test, y_train, y_test = train_test_split(class_data, class_column, test_size = 0.2)
-
-print(y_train.head())
-print(y_test.head())
-
-# Decision Trees
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(x_train,y_train)
-predictions = clf.predict(x_test)
-print(predictions[0:5])
-print(clf.score(y_test,predictions))
+for i in range(1,10):
+    x_train, x_test, y_train, y_test = train_test_split(class_data, class_column, test_size = i/10)
+    
+    # Decision Trees
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(x_train,y_train)
+    predictions = clf.predict(x_test)
+    print('test size:',i/10,'accuracy:',accuracy_score(y_test,predictions))
