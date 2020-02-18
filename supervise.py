e24de84d7bcbdac927d834754e599429ea2d22f2
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-18 21:50:24 +0000

    added another method

--- supervise.py
+++ supervise.py
@@ -3,15 +3,18 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import tree
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 dataset, classcolumn, headers, folds = sys.argv[1:]
 headers = None if headers == "None" else headers
 classcolumn = int(classcolumn) if headers == None else classcolumn
 folds = int(folds)
+
+neighbors = 3
 
 data = pd.read_csv(dataset, header=headers)
 class_column = data[classcolumn]
@@ -20,7 +23,11 @@ class_data = data.drop(classcolumn,1)
 # folds = 50
 kf = KFold(n_splits=folds, shuffle=True)
 count = 1
-accuracy_total = 0
+accuracies = {}
+classifiers = [('Decision Tree',DecisionTreeClassifier()),('kNN',KNeighborsClassifier(n_neighbors=neighbors))]
+for clf_name, classifier in classifiers:
+    accuracies[clf_name] = {}
+    accuracies[clf_name]['total'] = 0
 for train_index, test_index in kf.split(class_data):
     # print("TRAIN:", train_index, "TEST:", test_index)
 
@@ -28,12 +35,17 @@ for train_index, test_index in kf.split(class_data):
     x_train, y_train = class_data.iloc[train_index], class_column.iloc[train_index]
     x_test, y_test = class_data.iloc[test_index], class_column.iloc[test_index]
 
+
     # Decision Trees
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(x_train,y_train)
-    predictions = clf.predict(x_test)
-    accuracy = accuracy_score(y_test,predictions)
-    print('fold:',count,'accuracy:',accuracy)
-    count +=1 
-    accuracy_total += accuracy
-print("average_accuracy for",folds,"folds is:",accuracy_total/folds)
+    for clf_name, classifier in classifiers:
+        clf = classifier
+        clf = clf.fit(x_train,y_train)
+        predictions = clf.predict(x_test)
+        accuracy = accuracy_score(y_test,predictions)
+        # print('fold:',count,'accuracy:',accuracy)
+        count +=1 
+        accuracies[clf_name]['total'] += accuracy
+    
+for clf_name, classifier in classifiers:
+    accuracies[clf_name]['average'] = accuracies[clf_name]['total'] / folds
+    print('average_accuracy for classifier"',clf_name,'"with',folds,"folds is:",accuracies[clf_name]['average'])
