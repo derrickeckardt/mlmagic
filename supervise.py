4b38d0f9d62b6d029cdae6e332c7f5eb33637f2e
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-18 21:29:01 +0000

    added cross validation

--- supervise.py
+++ supervise.py
@@ -5,6 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+
 
 dataset, classcolumn, headers = sys.argv[1:]
 headers = None if headers == "None" else headers
@@ -14,11 +16,23 @@ data = pd.read_csv(dataset, header=headers)
 class_column = data[classcolumn]
 class_data = data.drop(classcolumn,1)
 
-for i in range(1,10):
-    x_train, x_test, y_train, y_test = train_test_split(class_data, class_column, test_size = i/10)
-    
+folds = 10
+kf = KFold(n_splits=folds, shuffle=True)
+count = 1
+accuracy_total = 0
+for train_index, test_index in kf.split(class_data):
+    # print("TRAIN:", train_index, "TEST:", test_index)
+
+    # x_train, x_test, y_train, y_test = train_test_split(class_data, class_column, test_size = i/10)
+    x_train, y_train = class_data.iloc[train_index], class_column.iloc[train_index]
+    x_test, y_test = class_data.iloc[test_index], class_column.iloc[test_index]
+
     # Decision Trees
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(x_train,y_train)
     predictions = clf.predict(x_test)
-    print('test size:',i/10,'accuracy:',accuracy_score(y_test,predictions))
+    accuracy = accuracy_score(y_test,predictions)
+    print('fold:',count,'accuracy:',accuracy)
+    count +=1 
+    accuracy_total += accuracy
+print("average_accuracy for",folds,"folds is:",accuracy_total/folds)
