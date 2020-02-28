88c2347ae5aaef89e51e12b5a179733031e30c47
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-02-28 20:21:49 +0000

    not sure how to clear out na values

--- data.py
+++ data.py
@@ -7,28 +7,32 @@ def create_column_class(dataset, classcolumn, headers):
     
     # Read in Datafile
     missing_values = get_missing_values()
-    data = pd.read_csv(dataset, header=headers, na_values=missing_values)
-    data = basic_clean_data(data)
+    raw_data = pd.read_csv(dataset, header=headers, na_values=missing_values)
+    data = basic_clean_data(raw_data)
     class_column = data[classcolumn]
     class_data = data.drop(classcolumn,1)
     return data, class_data, class_column
 
 def get_missing_values():
     # Eventually add ability to determine which missing values
-    missing_values = ["n/a", "N/A","N/a","n/A","na","NA","Na","nA","NAN","-"]
+    missing_values = ["n/a", "N/A","N/a","n/A","na","NA","Na","nA","NAN","-",""]
+    # issue warning about other ways it will not catch
     return missing_values
 
+def drop_sparse_columns():
+    
+    return 
+
 def basic_clean_data(data):
-    # clear blanks, empty strings, NaN, N/A
-    data = data.applymap(lambda x: np.nan if isinstance(x, str) and (not x or x.isspace()) else x)
-    # does not change 'Na' or 'NAN' or -
-    print(data.head())
+    # First, identify how many
+    threshold = 0.05
+    na_values = data.isna().sum()
+    data_shape = data.shape()
+    print(data_shape)
     
-    # issue warning about other ways it will not catch
-    
-    # Null, NaN, None
-    # Since data already loaded
-    
+    for column, na_count in zip(data.columns, na_values):
+        if na_count < data_shape[0]*threshold
+
     # sparse columns
         # dropping columns with na
     # Missing rows
@@ -37,8 +41,11 @@ def basic_clean_data(data):
     # Look for data that is out of place, a number when all th other respons are yes no
     
     # Extreme outliers
-    missing_values = data.isna().sum().sum()
-    print(missing_values)
+    
+
+    
+    print(na_values)
+    
     
     ## advanced cleaning
     # Address formatting
