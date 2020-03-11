0d19a9b0ce5b982c8fe830e29b1bf24bfed26572
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-03-11 19:54:00 +0000

    code cleanup

--- index.py
+++ index.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from statistics import stdev, mean
 import cProfile
 import supervise
